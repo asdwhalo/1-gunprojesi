dda73172886fc75dbd578250e4e0d0da740a92b4
asdwhalo <unuvarhalil01@gmail.com>
2026-02-19 18:21:56 +0300

    stuff

--- geo_dash/geo_cube.gd
+++ geo_dash/geo_cube.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
+class_name GeoCube
 
 @onready var ray: RayCast2D = $obstakableRay
 @onready var spr: Sprite2D = $"8Pix"
 @onready var main :Node2D = get_parent()
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 @export var speed:float = 100
 @export var jump_velocity:float = 50
@@ -17,9 +19,14 @@ func _physics_process(delta: float) -> void:
 	#yer çekimi
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		anim.play("jump")
+		
 	#zıplama
-	elif Input.is_action_just_pressed("accept"):
+	elif Input.is_action_just_pressed("accept") and can_move:
 		velocity.y -= jump_velocity
+		anim.stop()
+		anim.play("idle")
+		
 	else:
 		velocity.y = 0
 	# Nesneye çarpana kadar ilerleme
@@ -29,3 +36,7 @@ func _physics_process(delta: float) -> void:
 		can_move = false
 		main.cube_collided.emit()
 	move_and_slide()
+
+
+func _onmain_cube_collided() -> void:
+	can_move = false
